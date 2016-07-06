71089335a32feb85c25eba0d4bd15d367a5835a0
Fearox <alan.s.sze@gmail.com>
2016-07-05 22:29:46 -0400

    Code to run
    Have this in functions subfolder and add this to path for folder
    containing all the data

---
+++ MacroScript.m
@@ -0,0 +1,33 @@
+names1 = {'Chrona_NapEnt001sess1.edf','Chrona_NapEnt002sess1.edf','Chrona_NapEnt003sess1.edf','Chrona_NapEnt004sess1.edf','Chrona_NapEnt005sess1.edf','Chrona_NapEnt006sess1.edf','Chrona_NapEnt007sess1.edf','Chrona_NapEnt008sess1.edf','Chrona_NapEnt009sess1.edf','Chrona_NapEnt010sess1.edf','Chrona_NapEnt011sess1.edf'};
+names2 = {'Chrona_NapEnt001sess2.edf','Chrona_NapEnt002sess2.edf','Chrona_NapEnt003sess2.edf','Chrona_NapEnt004sess2.edf','Chrona_NapEnt005sess2.edf','Chrona_NapEnt006sess2.edf','Chrona_NapEnt007sess2.edf','Chrona_NapEnt008sess2.edf','Chrona_NapEnt009sess2.edf','Chrona_NapEnt010sess2.edf','Chrona_NapEnt011sess2.edf'};
+
+channelList = [4,5,6,7,8,9];
+
+truthTable = [0,1,1,0,0,0,1,1,0,1,1,0,1,0,0,0,1,0,1,1,1,1,0];
+
+powerTableTrue = zeros(length(names1),length(channelList),4);
+powerTableFalse = zeros(length(names1),length(channelList),4);
+
+bands = [0.5, 4; 5, 7; 8, 13; 14, 25];
+
+for i = 1:length(names1)
+    
+    figure(i)
+    
+    if (~(i == 1) && ~(i == 5) && ~(i == 6))
+        
+        for j = channelList
+            
+            subplot(2,3,j-3)
+            
+            [pxxT,fT,pxxF,fF] = SleepStudy(names1{i}(14:16),200,1000,500,names1{i},names2{i},j,truthTable(i));
+            for k = 1:4
+                powerTableTrue(i,j,k) = bandpower(pxxT,fT,bands(k,:),'psd');
+                powerTableFalse(i,j,k) = bandpower(pxxF,fF,bands(k,:),'psd');                
+            end
+            
+        end
+        
+    end
+    
+end
---
+++ SleepStudy.m
@@ -0,0 +1,36 @@
+function [pxxT, fT, pxxF, fF] = SleepStudy(ID,frequency,window,overlap,fname1,fname2,channel,polarity)
+
+str = horzcat('Patient: ',ID,' - ','Channel: ',num2str(channel));
+
+[hdr1, data1] = edfread(fname1);
+[hdr2, data2] = edfread(fname2);
+
+ChannelData1 = data1(channel,:);
+ChannelData2 = data2(channel,:);
+
+[S1,f1] = pwelch(ChannelData1,window,overlap,[],frequency);
+[S2,f2] = pwelch(ChannelData2,window,overlap,[],frequency);
+
+if polarity
+    plot(f1,10*log10(S1))
+    hold on
+    plot(f2,10*log10(S2))
+    pxxT = S1;
+    fT = f1;
+    pxxF = S2;
+    fF = f2;    
+else
+    plot(f2,10*log10(S2))
+    hold on
+    plot(f1,10*log10(S1))
+    pxxT = S2;
+    fT = f2;
+    pxxF = S1;
+    fF = f1;     
+end
+
+xlabel('Frequency (Hz)')
+ylabel('Magnitude (dB)')
+title(str)
+legend('Experimental','Control')
+
